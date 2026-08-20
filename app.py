29283babad5a206e6a1e5f1aa7b129636f679169
odiodespertar <odiodespertar@gmail.com>
2026-08-20 04:39:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if st.session_state.flotar_activo:
             }
 
             table, div[data-testid="stTable"], .js-plotly-plot {
-                max-height: 250px !important;
+                max-height: 380px !important;
                 overflow-y: auto !important;
                 display: block !important;
             }
