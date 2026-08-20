5b8143c28eda20cf5e5253fc82e85337c9282d4a
odiodespertar <odiodespertar@gmail.com>
2026-08-20 04:06:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -231,8 +231,8 @@ div[data-testid="stExpander"] > div[role="group"] {
 div[data-testid="stExpander"] summary {
     background: linear-gradient(
         135deg,
-        #1c2a39,
-        #1a3438
+        #1a484f,
+        #008B8B
     ) !important;
 
     padding: 15px 18px !important;
