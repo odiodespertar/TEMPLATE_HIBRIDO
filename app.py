72fdb23229c878ff5da81c659b7a239774f0ca4b
odiodespertar <odiodespertar@gmail.com>
2026-09-07 00:38:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1491,34 +1491,19 @@ NOMBRES_PLANES_C1_VACIA = [
 
 # --- DATOS NUEVOS PARA C1 SMD1 ---
 u_C1_SMD1 = { 
-    "Car MLP": [110, 120],
-    "Small Van MLP": [110, 120],
-    "Large Van MLP": [110, 120],
-    "Small Van MLP Newbie": [110, 120],
-    "Large Van MLP Newbie": [110, 120],
     "Extra large Van MLP": [110, 120],
-    "Small Van MLP XPT": [110, 120],
     "Small Van MLP foráneo": [110, 120],
     "Large Van MLP foráneo": [110, 120],
     "Large Van MLP Bulk": [100, 100],
     "Extra large Van MLP H&B": [50, 50],
-    "Rental Car": [120, 150],
     "Rental Electric Large Van": [120, 150],
     "Rental Large Van": [120, 150],
     "Rental Replacement": [120, 150],
-    "Rental Small Van Electrica": [120, 150],
-    "Rental Small Van": [120, 150],
-    "Delivery Cells Car": [1, 1],
-    "Truck 3.5 tons MLP": [1, 1],
     "Delivery Cell Large Van": [1, 1],
     "Car 8h": [70, 70],
     "Car Newbie": [50, 50],
     "Car Zona Ext 10h": [70, 70],
     "Moto 3h": [30, 30],
-    "Moto Newbie": [25, 25],
-    "Small Van 11h Ext": [70, 70],
-    "Small Van 9h": [70, 70],
-    "Small Van 9h Ext": [70, 70],
     "Small Van Newbie": [70, 70]
 }
 
