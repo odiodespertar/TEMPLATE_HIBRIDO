dd93e8cf59d563d2d5524a96b191d1c136112143
odiodespertar <odiodespertar@gmail.com>
2026-09-20 00:31:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3559,7 +3559,7 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
         
         nuevaInput.type = 'number';
         nuevaInput.className = 'in-2pct-lista';
-        nuevaInput.placeholder = `Dato ${numCeldas}`;
+        nuevaInput.placeholder = `Dato ${{numCeldas}}`;
         nuevaInput.onkeydown = function(e) {{ navegarConFlechas2Pct(e, this); }};
         nuevaInput.oninput = calcularDosPctDato1Dinámico;
         nuevaInput.onfocus = function() {{ this.select(); }};
